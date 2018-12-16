5f4815322989cdeb50521aa175cd0ebf1801aac2
Subhomoy Pal <subhomoyprojects@gmail.com>
2018-12-17 03:23:02 +0530

    Add component header,footer,index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { BarlistingComponent } from './superadmin/barlisting/barlisting.component';
+import { HeaderComponent } from './superadmin/header/header.component';
+import { FooterComponent } from './superadmin/footer/footer.component';
+import { IndexComponent } from './superadmin/index/index.component';
+import { SidenavbarComponent } from './superadmin/sidenavbar/sidenavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarlistingComponent
+    BarlistingComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    SidenavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 /* Component */
 import { BarlistingComponent } from '../superadmin/barlisting/barlisting.component';
+import { IndexComponent } from '../superadmin/index/index.component';
 import { from } from 'rxjs';
 
 const routes: Routes = [
   { path: '', redirectTo: '/superadmin/barlisting', pathMatch: 'full' },
   { path: 'superadmin/barlisting', component: BarlistingComponent },
+  { path: 'superadmin/index', component: IndexComponent }
 ];
 
 @NgModule({
