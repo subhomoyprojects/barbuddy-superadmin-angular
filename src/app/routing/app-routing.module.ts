430dc7f371983639af88533507b4f654dbdfa310
Subhomoy Pal <subhomoyprojects@gmail.com>
2019-01-07 13:25:20 +0530

    ADD bardetails link

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 /* Component */
 import { BarlistingComponent } from '../superadmin/barlisting/barlisting.component';
 import { IndexComponent } from '../superadmin/index/index.component';
+import { BardetailsComponent } from '../superadmin/bardetails/bardetails.component';
 import { from } from 'rxjs';
 
 const routes: Routes = [
   { path: '', redirectTo: '/superadmin/barlisting', pathMatch: 'full' },
   { path: 'superadmin/barlisting', component: BarlistingComponent },
-  { path: 'superadmin/index', component: IndexComponent }
+  { path: 'superadmin/index', component: IndexComponent },
+  { path: 'superadmin/bardetails', component: BardetailsComponent }
 ];
 
 @NgModule({
