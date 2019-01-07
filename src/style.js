7e2f1f59b5945128787a641dae7630c04a600642
Subhomoy Pal <subhomoyprojects@gmail.com>
2019-01-07 11:50:06 +0530

    Add style for responsive

--- src/style.js
+++ src/style.js
@@ -1,4 +1,10 @@
 $( document ).ready(function() {
+
+    if ($(window).width() < 768) {
+        $(".open-close-sidebar").addClass("toggled");
+        $("body").addClass("sidebar-toggled");
+    }
+
     $(".open-close-sidenav").click(function(){
         $(".open-close-sidebar").toggleClass("toggled");
     });
