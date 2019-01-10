1f1101c4fe4e17119a0ed9606b2dd20f8d4482bb
Subhomoy Pal <subhomoyprojects@gmail.com>
2019-01-10 16:48:21 +0530

    Remove slider

--- src/app/superadmin/moreinfo/moreinfo.component.ts
+++ src/app/superadmin/moreinfo/moreinfo.component.ts
@@ -11,26 +11,4 @@ export class MoreinfoComponent implements OnInit {
 
   ngOnInit() {
   }
-  customOptions: any = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
-    navSpeed: 700,
-    navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      }
-    },
-    nav: true
-  }
-
 }
