ea33d1a41dbc85319cc9ffcf74b844d95cf15839
Subhomoy Pal <subhomoyprojects@gmail.com>
2019-01-10 17:06:26 +0530

    modify owl

--- src/app/superadmin/moreinfo/moreinfo.component.ts
+++ src/app/superadmin/moreinfo/moreinfo.component.ts
@@ -11,4 +11,28 @@ export class MoreinfoComponent implements OnInit {
 
   ngOnInit() {
   }
+  customOptions: any = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    autoWidth:true,
+    margin:10,
+    navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      }
+    },
+    nav: true
+  }
+
 }
