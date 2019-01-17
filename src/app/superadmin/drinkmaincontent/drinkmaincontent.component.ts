fc64a1fa7bfbb3a8788457a1979e0d1b97992db5
Subhomoy Pal <subhomoyprojects@gmail.com>
2019-01-17 12:58:43 +0530

    Add Manually drink Modal

--- src/app/superadmin/drinkmaincontent/drinkmaincontent.component.ts
+++ src/app/superadmin/drinkmaincontent/drinkmaincontent.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbTabsetConfig} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-drinkmaincontent',
   templateUrl: './drinkmaincontent.component.html',
-  styleUrls: ['./drinkmaincontent.component.css']
+  styleUrls: ['./drinkmaincontent.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class DrinkmaincontentComponent implements OnInit {
 
   ngOnInit() {
   }
 
-  constructor(config: NgbTabsetConfig) {
+  constructor(config: NgbTabsetConfig, drinkmodal: NgbModalConfig, private modalService: NgbModal) {
     config.justify = 'center';
+    drinkmodal.backdrop = 'static';
+    drinkmodal.keyboard = false;
    }
+
+   open(addDrinkManuallyModal) {
+    this.modalService.open(addDrinkManuallyModal);
+  }
 
 }
