c1239b677509d05bf96236de7dde84fe3373d2be
Subhomoy Pal <subhomoyprojects@gmail.com>
2019-01-19 12:12:47 +0530

    Change offer page

--- src/app/superadmin/offers/offers.component.ts
+++ src/app/superadmin/offers/offers.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
+import { MatExpansionPanel } from '@angular/material';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.component.html',
-  styleUrls: ['./offers.component.css']
+  styleUrls: ['./offers.component.css'],
+  providers: [NgbDropdownConfig] // add NgbDropdownConfig to the component providers
 })
 export class OffersComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('myPanel') myPanel: MatExpansionPanel;
+    matIcon = 'keyboard_arrow_down' || 'keyboard_arrow_up';
+
+  @ViewChild('myPanel1') myPanel1: MatExpansionPanel;
+  matIcon1 = 'keyboard_arrow_down' || 'keyboard_arrow_up';
+
+  constructor(config: NgbDropdownConfig) { 
+    config.placement = 'bottom-right';
+    config.autoClose = false;
+  }
+   // Metarial Accordine
+   panelOpenState = false;
 
   ngOnInit() {
+    this.myPanel.expandedChange.subscribe((data) => {
+      this.matIcon = data ? 'keyboard_arrow_up' : 'keyboard_arrow_down';
+    });
+    this.myPanel1.expandedChange.subscribe((data) => {
+      this.matIcon1 = data ? 'keyboard_arrow_up' : 'keyboard_arrow_down';
+    });
+  }
+
+  // Metarial Accordine Axpend by click arrow button
+  expandPannel() {
+    this.myPanel.expanded = !this.myPanel.expanded;
+  }
+  expandPannel1() {
+    this.myPanel1.expanded = !this.myPanel1.expanded;
   }
 
 }
